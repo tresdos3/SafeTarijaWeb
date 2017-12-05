c33a19d0f351f95f6ee7865aa68f97791c42234e
Luis Juarez <tresdosasd@gmail.com>
2017-12-05 00:09:45 -0400

    Add Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { SigninComponent } from './content/signin/signin.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: SigninComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
