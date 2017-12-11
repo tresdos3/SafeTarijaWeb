e22e9a4c6f37f8f101d22ac104432b70de51004e
Luis Juarez <tresdosasd@gmail.com>
2017-12-11 10:47:14 -0400

    Add Dashboad v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,12 +10,14 @@ import { LostpasswordComponent } from './content/lostpassword/lostpassword.compo
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
-import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
 import { DashboardComponent } from './content/dashboard/dashboard.component';
 import { AuthService } from './core/service/auth.service';
 import { NavbarComponent } from './content/component/navbar/navbar.component';
 import { FooterComponent } from './content/component/footer/footer.component';
 import { SidebarComponent } from './content/component/sidebar/sidebar.component';
+import { WelcomeComponent } from './content/dashboard/welcome/welcome.component';
+import { ProfileComponent } from './content/dashboard/profile/profile.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +27,9 @@ import { SidebarComponent } from './content/component/sidebar/sidebar.component'
     DashboardComponent,
     NavbarComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    WelcomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +50,8 @@ import { SidebarComponent } from './content/component/sidebar/sidebar.component'
     RouterModule.forRoot([
       {
         path: '',
-        component: SigninComponent
+        component: SigninComponent,
+        pathMatch: 'full'
       },
       {
         path: 'signup',
@@ -58,7 +63,22 @@ import { SidebarComponent } from './content/component/sidebar/sidebar.component'
       },
       {
         path: 'dashboard',
-        component: DashboardComponent
+        component: DashboardComponent,
+        children: [
+          {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'welcome'
+          },
+          {
+            path: 'welcome',
+            component: WelcomeComponent
+          },
+          {
+            path: 'profile',
+            component: ProfileComponent
+          }
+        ]
       }
     ])
   ],
--- src/app/content/dashboard/dashboard.component.ts
+++ src/app/content/dashboard/dashboard.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import 'rxjs/add/operator/filter';
+import { NavbarComponent } from '../component/navbar/navbar.component';
+import { Subscription } from 'rxjs/Subscription';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent } from '@angular/common';
+import PerfectScrollbar from 'perfect-scrollbar';
+import { AuthService } from '../../core/service/auth.service';
+declare const $: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +15,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  private _router: Subscription;
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
+  @ViewChild(NavbarComponent) navbar: NavbarComponent;
+  constructor(private Auth: AuthService, private afAuth: AngularFireAuth, private router: Router, private location: Location) { }
 
   ngOnInit() {
+    $.material.init();
+    const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+    const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+    this.location.subscribe((ev: PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+    this.router.events.subscribe((event: any) => {
+      this.navbar.sidebarClose();
+      if (event instanceof NavigationStart) {
+        // tslint:disable-next-line:curly
+        if (event.url !== this.lastPoppedUrl)
+          this.yScrollStack.push(window.scrollY);
+      } else if (event instanceof NavigationEnd) {
+        if (event.url === this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        // tslint:disable-next-line:curly
+        } else
+          window.scrollTo(0, 0);
+      }
+    });
+    this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+      elemMainPanel.scrollTop = 0;
+      elemSidebar.scrollTop = 0;
+    });
+    if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+      let ps = new PerfectScrollbar(elemMainPanel);
+      ps = new PerfectScrollbar(elemSidebar);
+    };
+    this.afAuth.authState.subscribe(user => {
+      if (user === null) {
+        this.router.navigate(['/']);
+      }
+    });
   }
-
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+    this.runOnRouteChange();
+  }
+  isMaps(path) {
+    let titlee = this.location.prepareExternalUrl(this.location.path());
+    titlee = titlee.slice(1);
+    if (path === titlee) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  runOnRouteChange(): void {
+    if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+      const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+      const ps = new PerfectScrollbar(elemMainPanel);
+      ps.update();
+    }
+  }
+  isMac(): boolean {
+    let bool = false;
+    if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+      bool = true;
+    }
+    return bool;
+  }
+  logout() {
+    this.Auth.SignOut();
+  }
 }
